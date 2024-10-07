fa246861a1e5248dfad59777ade90ec11a9fefb4
Programming Rainbow <159774797+ProgrammingRainbow@users.noreply.github.com>
2024-10-07 10:39:09 +0100

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,6 @@ o.tabstop = 4
 o.softtabstop = 4
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- set filetype for .CBL COBOL files.
+-- vim.cmd([[ au BufRead,BufNewFile *.CBL set filetype=cobol ]])
