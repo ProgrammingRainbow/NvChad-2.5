1e1c1193ec181776d27546b1b05498d326ff6a92
Donald Williams <129223418+dwilliam62@users.noreply.github.com>
2024-10-31 18:21:45 -0400

    Update mappings.lua
    Now x won't paste deleted character to clipboard

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "x", "_xx")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
