ef0a2e2dac5305fcd8d9e65c9d94273f06c7912c
Programming Rainbow <159774797+ProgrammingRainbow@users.noreply.github.com>
2025-01-26 11:11:54 +0000

    Update lint.lua

--- lua/configs/lint.lua
+++ lua/configs/lint.lua
@@ -7,10 +7,14 @@ lint.linters_by_ft = {
 }
 
 lint.linters.luacheck.args = {
-    unpack(lint.linters.luacheck.args),
     "--globals",
     "love",
     "vim",
+    "--formatter",
+    "plain",
+    "--codes",
+    "--ranges",
+    "-",
 }
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
